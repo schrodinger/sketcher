69b99a4ba764e6e315e7224cc5bba644b38f03bd
Ethan Alguire <ethan.alguire@schrodinger.com>
2022-02-15 17:21:15 -0500

    SKETCH-1231, SKETCH-1344: move enumeration tools out of fragment tool widget. Add R groups to Edit Atom Properties dialog.

---
+++ src/schrodinger/sketcher/sketcher_css_style.h
@@ -0,0 +1,12 @@
+#include <QString>
+
+namespace schrodinger
+{
+namespace sketcher
+{
+
+const QString TOOL_BUTTON_STYLE{
+    "QToolButton:checked { background-color: rgb(161, 189, 236); }"};
+
+} // sketcher
+} // schrodinger
