c8606f1c84a1c8b25b65f02ccccf97934d99ba47
Chris Von Bargen <christopher.vonbargen@schrodinger.com>
2022-03-09 00:16:06 -0500

    SKETCH-1436. Update palette text colors, disabled color; add hover highlight for tool buttons; disable Draw Tools title in Erase mode; fix Paste in Text orange color; add separator between ring and enum palettes; all tooltip for All selection button

--- src/schrodinger/sketcher/sketcher_css_style.h
+++ src/schrodinger/sketcher/sketcher_css_style.h
@@ -5,8 +5,10 @@ namespace schrodinger
 namespace sketcher
 {
 
+// TODO: These are LiveDesign colors; add Maestro colors in SKETCH-1342
 const QString TOOL_BUTTON_STYLE{
-    "QToolButton:checked { background-color: rgb(161, 189, 236); }"};
+    "QToolButton:checked { background-color: #b8d7ac; }\n"
+    "QToolButton:hover:!checked { background-color: #daebd5 ; }\n"};
 
 } // sketcher
 } // schrodinger
