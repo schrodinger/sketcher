a58c6eeeef9859c78b7fd3c12d81b3e113bd839f
Anderson Chan <anderson.chan@schrodinger.com>
2022-04-08 15:42:31 -0400

    SKETCH-1230: Make chooser triangles look more disabled when button is disabled

--- src/schrodinger/sketcher/sketcher_css_style.h
+++ src/schrodinger/sketcher/sketcher_css_style.h
@@ -7,6 +7,24 @@ namespace sketcher
 
 const QString DISABLED_COLOR{"#b6b6b6"};
 
+const QString TOOL_BUTTON_WITH_POPUP_BASE_STYLE{"QToolButton {"
+                                                " border : none;"
+                                                " padding: 0px;"
+                                                " margin: 0px;"
+                                                " spacing: 0px;"
+                                                "}"};
+
+const QString TOOL_BUTTON_WITH_POPUP_AND_ARROW_STYLE =
+    TOOL_BUTTON_WITH_POPUP_BASE_STYLE +
+    QString{"QToolButton {"
+            " background-image:url(\":icons/corner_arrow.png\"); "
+            " background-position: bottom right; "
+            " background-repeat: no-repeat; "
+            "}"
+            "QToolButton:disabled {"
+            " background-image:url(\":icons/disabled_corner_arrow.png\"); "
+            "}"};
+
 // TODO: These are LiveDesign colors; add Maestro colors in SKETCH-1342
 const QString TOOL_BUTTON_STYLE{
     "QToolButton:checked { background-color: #b8d7ac; }\n"
