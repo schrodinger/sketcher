8ff21a1324d35134f235fd0ad60de2bf2db72c4c
Anderson Chan <anderson.chan@schrodinger.com>
2022-04-13 11:25:56 -0400

    SKETCH-1553: Fix popup borders on WASM sketcher build

--- src/schrodinger/sketcher/sketcher_css_style.h
+++ src/schrodinger/sketcher/sketcher_css_style.h
@@ -27,10 +27,13 @@ const QString TOOL_BUTTON_WITH_POPUP_AND_ARROW_STYLE =
 
 // TODO: These are LiveDesign colors; add Maestro colors in SKETCH-1342
 const QString TOOL_BUTTON_STYLE{
-    "QToolButton:checked { background-color: #b8d7ac; }\n"
-    "QToolButton:hover:!checked { background-color: #daebd5 ; }\n"};
+    "QToolButton:checked { background-color: rgba(184, 215, 172, .65); }\n"
+    "QToolButton:hover:!checked { background-color: rgba(218, 235, 213, .5) ; "
+    "}\n"};
 
 // TODO: Extract the general Sketcher background color into a constant.
-const QString WINDOW_STYLE{"QMainWindow { background-color: white; }\n"};
+const QString GENERAL_STYLE{
+    "QMainWindow { background-color: white; }\n"
+    "schrodinger--sketcher--ModularPopup { background-color: white }"};
 } // namespace sketcher
 } // namespace schrodinger
