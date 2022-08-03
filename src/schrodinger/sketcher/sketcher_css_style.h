54c016f7f8f7b7f3c3be04d193e04003f01bd804
Chris Von Bargen <christopher.vonbargen@schrodinger.com>
2022-08-03 12:06:49 -0400

    SKETCH-1704. Hook up the square/lasso selection buttons; change palette background color on active selection; assure that the sketcher behavior matches Laura's spec

--- src/schrodinger/sketcher/sketcher_css_style.h
+++ src/schrodinger/sketcher/sketcher_css_style.h
@@ -13,6 +13,8 @@ const QString GENERAL_STYLE{
     "QMainWindow { background-color: white; }"
     "schrodinger--sketcher--ModularPopup { background-color: white; }"};
 
+const QString SELECTION_ACTIVE_STYLE{"QWidget { background-color: #f3f6f0; }"};
+
 const QString TOOL_BUTTON_STYLE{
     "QToolButton { border: none; }"
     "QToolButton:disabled { color: #E4E4E4; }"
