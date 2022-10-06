43d606f11687ccc0772fcfe5e25b8075e187ab0c
Chris Von Bargen <christopher.vonbargen@schrodinger.com>
2022-10-06 09:26:45 -0400

    SKETCH-1349. Redesign Save Image dialog, and make it available in the WASM build as well

--- src/schrodinger/sketcher/sketcher_css_style.h
+++ src/schrodinger/sketcher/sketcher_css_style.h
@@ -64,6 +64,9 @@ const QString PERIODIC_TABLE_STYLE{
     "QPushButton[class='actinides'] { background-color: #afd1a2; }"
     "QLabel { font: 10px; color: black; border-style: none; padding: 0; }"};
 
+const QString RENDER_OPTIONS_POPUP_STYLE{
+    "#FileSaveImagePopup { background-color:white }"};
+
 const QString BOND_QUERY_STYLE{
     "QToolButton { font: bold 12px; color: #333333; }"};
 
