8b59ef58d595732bcf380200768ce4f34e1193b5
Dan Nealschneider <dan.nealschneider@schrodinger.com>
2023-01-27 14:12:59 -0800

    Revert "SKETCH-1743: Make real structures from Sketcher not be queries"
    This reverts commit 82fb5eb36e3aa68ea44ffafb4683cecc7fc6e1c2.
    
    http://buildbot.dev.bb.schrodinger.com/builders/2023-1_per_commit_Linux-x86_64/builds/1401/steps/buildinger_--with-tests/logs/mmshare_make_all

--- src/schrodinger/sketcher/dialog/edit_atom_properties.cpp
+++ src/schrodinger/sketcher/dialog/edit_atom_properties.cpp
@@ -538,7 +538,7 @@ void EditAtomPropertiesDialog::writeAtomInfo()
     ui->atom_common_props_wdg->writeAtomInfo(m_atom);
 
     auto rdk_atom = m_atom.getRDKAtom();
-    if (rdk_atom != nullptr && rdk_atom->hasQuery()) {
+    if (rdk_atom != nullptr) {
         rdk_atom->setQuery(nullptr);
     }
 }
