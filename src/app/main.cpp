7d9116cae83ffd90d70a3dea599938732344cb21
Chris Von Bargen <christopher.vonbargen@schrodinger.com>
2023-08-01 16:28:23 -0400

    add fmt dependency, should break

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,7 +1,11 @@
-#include <cstdio> 
+#include <cstdio>
+
+#include <fmt/format.h>
 
 int main(int argc, char** argv)
 {
-    printf("Hello World!");
+    printf(fmt::format("The answer to life, the universe, and everything is {}",
+                       42)
+               .c_str());
     return 0;
 }
