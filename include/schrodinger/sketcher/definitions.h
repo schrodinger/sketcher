feec693e0c6481a017b0437809f7db09827359b1
Chris Von Bargen <christopher.vonbargen@schrodinger.com>
2024-01-30 11:57:36 -0500

    Francois recommended changes

--- include/schrodinger/sketcher/definitions.h
+++ include/schrodinger/sketcher/definitions.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#ifdef SKETCHER_BUILD_SHARED_LIBS
 #ifdef _WIN32
 #define API_HELPER_IMPORT __declspec(dllimport)
 #define API_HELPER_EXPORT __declspec(dllexport)
@@ -11,9 +12,10 @@
 #define API_HELPER_IMPORT __attribute__((visibility("default")))
 #define API_HELPER_EXPORT __attribute__((visibility("default")))
 #endif
+#endif
 
 #ifdef IN_SKETCHER_DLL
-#define SKETCHER_API
+#define SKETCHER_API API_HELPER_EXPORT
 #else
-#define SKETCHER_API
+#define SKETCHER_API API_HELPER_IMPORT
 #endif
