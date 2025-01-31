d9181cdb1762923df6f87e7f3ebad2bcacecfe25
Kevin Keating <kevin.keating@schrodinger.com>
2025-01-31 11:29:50 -0500

    Q_IMPORT_PLUGIN

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,8 +1,11 @@
 // @copyright Schrodinger, LLC - All Rights Reserved
 
 #include <QApplication>
+#include <QtPlugin>
 
 #include "schrodinger/sketcher/example_widget.h"
+
+Q_IMPORT_PLUGIN (QXcbIntegrationPlugin)
 
 int main(int argc, char** argv)
 {
