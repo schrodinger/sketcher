9ccfd8226b8a27c8359b5ef61f444fb6b339a647
Kevin Keating <kevin.keating@schrodinger.com>
2025-02-11 14:50:33 -0500

    unreverting definitions.h changes

--- include/schrodinger/sketcher/definitions.h
+++ include/schrodinger/sketcher/definitions.h
@@ -2,13 +2,8 @@
 
 #pragma once
 
-#ifdef _WIN32
-#define API_HELPER_IMPORT __declspec(dllimport)
-#define API_HELPER_EXPORT __declspec(dllexport)
-#else
 #define API_HELPER_IMPORT __attribute__((visibility("default")))
 #define API_HELPER_EXPORT __attribute__((visibility("default")))
-#endif
 
 #ifdef SKETCHER_STATIC_DEFINE
 #define SKETCHER_API
