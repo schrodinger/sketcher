a975beb446d7597ec955574d6f80bd852956c3fd
Kevin Keating <kevin.keating@schrodinger.com>
2025-02-11 15:25:18 -0500

    reverting stuff again

--- include/schrodinger/rdkit_extensions/definitions.h
+++ include/schrodinger/rdkit_extensions/definitions.h
@@ -2,8 +2,13 @@
 
 #pragma once
 
+#ifdef _WIN32
+#define API_HELPER_IMPORT __declspec(dllimport)
+#define API_HELPER_EXPORT __declspec(dllexport)
+#else
 #define API_HELPER_IMPORT __attribute__((visibility("default")))
 #define API_HELPER_EXPORT __attribute__((visibility("default")))
+#endif
 
 #ifdef RDKIT_EXTENSIONS_STATIC_DEFINE
 #define RDKIT_EXTENSIONS_API
