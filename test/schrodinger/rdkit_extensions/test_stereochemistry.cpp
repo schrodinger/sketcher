4c92ba1c8e2aaf174cd3e5295eb424c674dce824
Ricardo R-S <rodriguez@schrodinger.com>
2025-06-20 16:30:41 +0200

    SHARED-11495: add some testing

--- test/schrodinger/rdkit_extensions/test_stereochemistry.cpp
+++ test/schrodinger/rdkit_extensions/test_stereochemistry.cpp
@@ -9,6 +9,7 @@
 #include <rdkit/GraphMol/RWMol.h>
 
 #include "schrodinger/rdkit_extensions/convert.h"
+#include "schrodinger/rdkit_extensions/coord_utils.h"
 #include "schrodinger/rdkit_extensions/stereochemistry.h"
 
 BOOST_TEST_DONT_PRINT_LOG_VALUE(schrodinger::rdkit_extensions::EnhancedStereo)
@@ -72,6 +73,29 @@ BOOST_AUTO_TEST_CASE(test_set_enhanced_stereo_for_atom)
     // make sure that the absolute stereochemistry group was deleted now that
     // it's empty
     BOOST_TEST(mol->getStereoGroups().size() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(test_wedgeMolBondse)
+{
+    // SHARED-11495
+    auto mol = to_rdkit("C[C@H](Cl)F", rdkit_extensions::Format::SMILES);
+    compute2DCoords(*mol);
+
+    auto b = mol->getBondWithIdx(0);
+
+    // The chirality would be encoded with a dash bond
+    wedgeMolBonds(*mol, &mol->getConformer());
+    BOOST_REQUIRE_EQUAL(b->getBondDir(), RDKit::Bond::BondDir::BEGINDASH);
+
+    // Check that a misleading mol bond cfg property does
+    // not result in the wrong wedging.
+    b->setProp(RDKit::common_properties::_MolFileBondCfg, 1); // UP
+    wedgeMolBonds(*mol, &mol->getConformer());
+    BOOST_CHECK_EQUAL(b->getBondDir(), RDKit::Bond::BondDir::BEGINDASH);
+
+    // the outdated property should be removed after recalculating wedges
+    BOOST_CHECK_EQUAL(b->hasProp(RDKit::common_properties::_MolFileBondCfg),
+                      false);
 }
 
 } // namespace rdkit_extensions
