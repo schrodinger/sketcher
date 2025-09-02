6745496f0120b648eb20c4c7b5a6c8b28b725f21
Nic Zonta <nicola.zonta@schrodinger.com>
2025-09-02 10:49:17 +0200

    make queries respect label visibility preferences

--- src/schrodinger/sketcher/molviewer/atom_item.cpp
+++ src/schrodinger/sketcher/molviewer/atom_item.cpp
@@ -323,6 +323,7 @@ AtomItem::determineLabelType() const
             // bound BondItem knows not to make space for the "*" label
             label_is_visible = false;
         } else if (m_atom->hasQuery()) {
+            qDebug() << "Atom has query:" << m_atom->getQueryType();
 
             // Wildcard or list query. This needs to be after R groups and
             // attachment points, since it would trigger for both but we need to
@@ -347,6 +348,7 @@ AtomItem::determineLabelType() const
             main_label_text = m_atom->getSymbol();
         }
     } else if (m_atom->hasQuery()) {
+        label_is_visible = determineLabelIsVisible();
         main_label_text = m_atom->getSymbol();
         needs_additional_labels = true;
         // hide implicit Hs on queries
