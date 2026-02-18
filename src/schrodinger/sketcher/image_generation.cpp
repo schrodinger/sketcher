75fa1588190588646e65c348dc2de7d1ffb7ac8a
Kevin Keating <kevin.keating@schrodinger.com>
2026-02-18 11:27:45 -0500

    SKETCH-2682: Now loading fonts in image_generation (#241)
    * Linked Case: SKETCH-2682, SKETCH-2685
    
    ### Description
    We were loading the Arimo fonts when instantiating a `SketcherWidget`,
    but the `image_generation` API doesn't use a `SketcherWidget`. That
    means that if an `image_generation` function was called before a
    `SketcherWidget` had been instantiated in that same process, the image
    would be potentially be generated without the right font. This branch
    adds a new `load_font_resources()` function that gets called from
    `SketcherWidget` as well a handful of `image_generation` functions.
    
    ### Testing Done
    I tried to trigger the warning from SKETCH-2685 with an mmshare master
    build, but I wasn't able to. It's possible that the warning only gets
    generated on Mac, which is what happened with SKETCH-2547. To make sure
    that the new code was getting run when I expected, I moved the changes
    over to mmshare, added a print statement to `load_font_resources()`, and
    confirmed that the output was printed first time (and only the first
    time) a `SketcherWidget` was instantiated or an `image_generation`
    function was called.

--- src/schrodinger/sketcher/image_generation.cpp
+++ src/schrodinger/sketcher/image_generation.cpp
@@ -22,6 +22,7 @@
 #include <boost/bimap.hpp>
 #include <unordered_map>
 
+#include "schrodinger/sketcher/font_loader.h"
 #include "schrodinger/sketcher/model/sketcher_model.h"
 #include "schrodinger/sketcher/molviewer/scene.h"
 #include "schrodinger/sketcher/molviewer/constants.h"
@@ -264,6 +265,16 @@ void init_molviewer_image(MolModel& mol_model, SketcherModel& sketcher_model,
 }
 
 /**
+ * Return a new Scene after ensuring that the font has been loaded.
+ */
+Scene get_scene(MolModel* mol_model, SketcherModel* sketcher_model,
+                QWidget* parent = nullptr)
+{
+    load_font_resources();
+    return Scene(mol_model, sketcher_model, parent);
+}
+
+/**
  * Construct a paint device and render the given input to it
  *
  * @param input What to render into the scene.  Can be an RDKit molecule, an
@@ -281,7 +292,7 @@ paint_scene(const T& input, const RenderOptions& opts,
     QUndoStack undo_stack;
     MolModel mol_model(&undo_stack);
     SketcherModel sketcher_model;
-    Scene scene(&mol_model, &sketcher_model);
+    auto scene = get_scene(&mol_model, &sketcher_model);
     init_molviewer_image(mol_model, sketcher_model, input, opts);
 
     return paint_scene(scene, opts, instantiate_paint_device_to_size);
@@ -446,7 +457,7 @@ qreal get_image_scale_for_mol_or_rxn(const T& mol_or_rxn, RenderOptions opts)
     QUndoStack undo_stack;
     MolModel mol_model(&undo_stack);
     SketcherModel sketcher_model;
-    Scene scene(&mol_model, &sketcher_model);
+    auto scene = get_scene(&mol_model, &sketcher_model);
     opts.scale = AUTOSCALE;
     init_molviewer_image(mol_model, sketcher_model, mol_or_rxn, opts);
     return get_scale(scene.getSceneItemsBoundingRect(), opts.width_height);
@@ -536,7 +547,7 @@ QByteArray get_LiveDesign_image_bytes(const T& input, ImageFormat format,
     QUndoStack undo_stack;
     MolModel mol_model(&undo_stack);
     SketcherModel sketcher_model;
-    Scene scene(&mol_model, &sketcher_model);
+    auto scene = get_scene(&mol_model, &sketcher_model);
     init_molviewer_image(mol_model, sketcher_model, input, opts);
 
     return get_image_bytes<Scene>(scene, format, opts);
@@ -586,7 +597,7 @@ qreal get_best_image_scale(const QList<RDKit::ROMol*> all_rdmols,
     QUndoStack undo_stack;
     MolModel mol_model(&undo_stack);
     SketcherModel sketcher_model;
-    Scene scene(&mol_model, &sketcher_model);
+    auto scene = get_scene(&mol_model, &sketcher_model);
     for (auto rdmol : all_rdmols) {
         init_molviewer_image(mol_model, sketcher_model, *rdmol, opts);
         qreal cur_scale =
