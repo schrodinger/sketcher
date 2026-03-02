078bfb03ae683bd0c44458ebd1c1b1fc229374f5
Nic Zonta <nicola.zonta@schrodinger.com>
2026-03-02 18:18:32 +0100

    CRDGEN-401 fix bugs in resize code (#250)
    * Linked Case: CRDGEN-401 
    
    ### Description
    fix a bug in the clustering for resize operations and increase the
    threshold
    
    ### Testing Done
    generated coords for problematic structures, they all look good now.
    Will compile a full table once these cases are closed

--- src/schrodinger/rdkit_extensions/helm/monomer_coordgen.cpp
+++ src/schrodinger/rdkit_extensions/helm/monomer_coordgen.cpp
@@ -2162,8 +2162,8 @@ void assign_clusters(std::vector<MonomerResizeData>& resize_data,
 
         resize_data[i].*cluster_member = next_cluster_id;
         for (size_t j = i + 1; j < resize_data.size(); ++j) {
-            if (std::abs(resize_data[i].delta.*coord_member -
-                         resize_data[j].delta.*coord_member) < eps) {
+            if (std::abs(resize_data[i].ref_pos.*coord_member -
+                         resize_data[j].ref_pos.*coord_member) < eps) {
                 resize_data[j].*cluster_member = next_cluster_id;
             }
         }
@@ -2302,11 +2302,11 @@ std::vector<MonomerResizeData> collect_linear_resize_data(
 
     // group vertically stacked monomers to avoid compound horizontal shifts
     assign_clusters(result, &RDGeom::Point3D::x, &MonomerResizeData::x_cluster,
-                    MONOMER_MINIMUM_SIZE * 0.25);
+                    MONOMER_MINIMUM_SIZE);
 
     // group horizontally stacked monomers to avoid compound vertical shifts
     assign_clusters(result, &RDGeom::Point3D::y, &MonomerResizeData::y_cluster,
-                    MONOMER_MINIMUM_SIZE * 0.25);
+                    MONOMER_MINIMUM_SIZE);
 
     return result;
 }
@@ -2658,6 +2658,7 @@ void resize_monomers(RDKit::ROMol& mol,
 
     auto linear_resize_data =
         collect_linear_resize_data(mol, monomer_sizes, ring_resize_info);
+
     auto ring_resize_data =
         collect_ring_resize_data(mol, monomer_sizes, ring_resize_info);
     if (linear_resize_data.empty() && ring_resize_data.empty()) {
