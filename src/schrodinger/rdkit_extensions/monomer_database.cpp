c1aa0a3e3098084a0b9fbaefa96d89bc0994eb0e
Ivan Tubert-Brohman <ivan.tubert-brohman@schrodinger.com>
2026-09-25 10:37:24 -0400

    SHARED-12727: Catch sanitization error for tautomers

--- src/schrodinger/rdkit_extensions/monomer_database.cpp
+++ src/schrodinger/rdkit_extensions/monomer_database.cpp
@@ -1205,7 +1205,13 @@ MonomerDatabase::getComplexMonomerQueries() const
                     auto can_smiles = RDKit::MolToSmiles(tautomer);
                     std::unique_ptr<RDKit::RWMol> can_tautomer(
                         RDKit::SmilesToMol(can_smiles, debug, sanitize));
-                    RDKit::MolOps::sanitizeMol(*can_tautomer);
+                    try {
+                        RDKit::MolOps::sanitizeMol(*can_tautomer);
+                    } catch (const RDKit::MolSanitizeException&) {
+                        // Sometimes the tautomerization reactions produce
+                        // an invalid structure; skip it.
+                        continue;
+                    }
                     query.mol = make_query(*can_tautomer, can_smiles);
                     query.attch_map = make_attch_map(*query.mol);
                     query.name = monomer_id.symbol;
